5cdc963bbbb2597cab775086b686353a640aa31c
Andreas Hasselberg <andreas.hasselberg@gmail.com>
2008-03-01 00:11:25 +0000

    initial commit

---
+++ avl.erl
@@ -0,0 +1,294 @@
+%%%-------------------------------------------------------------------
+%%% File    : avl.erl
+%%% Author  : Andreas Hasselberg <andreas.hasselberg@gmail.com>
+%%% Description : testing avl trees
+%%%
+%%% Created : 26 Dec 2007 by Andreas Hasselberg <>
+%%%-------------------------------------------------------------------
+-module(avl).
+
+-define(depth(Node), element(3, Node)).
+-define(nil, {nil, nil, 0}).
+
+-export([empty/0, is_empty/1, size/1, lookup/2, get/2, insert/3,
+	 delete/2, delete_any/2, balance/1,
+	 keys/1, 
+	 smallest/1, largest/1, take_smallest/1, take_largest/1]).
+
+-export([depth/1, test_all/0]).
+
+-compile([export_all]).
+
+%% update/3, enter/3, from_orddict/1, iterator/1, next/1, is_defined/2
+%% to_list/1, values/1, 
+
+
+%% node
+%%
+%% {
+%%  key, %% term
+%%  val, %% term
+%%  depth, %% int
+%%  left, %% node()
+%%  right %% node()
+%% }
+%%
+
+empty() -> ?nil.
+
+is_empty(?nil) -> true;
+is_empty(_) -> false.
+
+
+%% the excecution time of size/1 is linear to the size of the tree
+size(T) ->
+    size1(T).
+
+size1(?nil) -> 0;    
+size1({_Key, _Val, _Depth, TreeR, TreeL}) ->
+    1 + size1(TreeR) + size1(TreeL).
+    
+depth(T) -> ?depth(T).
+
+%%done
+lookup(FKey, {Key, Val, _, _, _} = T) ->
+    lookup(FKey, T, Key, Val).
+
+lookup(FKey, {Key, _V, _D, TreeL, _TR}, CKey, CVal) when FKey < Key ->
+    lookup(FKey, TreeL, CKey, CVal);
+lookup(FKey, {Key, Val, _D, _TL, TreeR}, _CKey, _CVal) ->
+    lookup(FKey, TreeR, Key, Val);
+lookup(FKey, ?nil, FKey, Val) -> {value, Val};
+lookup(_, ?nil, _, _) -> none.
+
+get(Key, Tree) ->
+    {value, Val} = lookup(Key, Tree),
+    Val.
+
+%% balance({_Key, _Val, _Dept, TreeL, TreeR} = T) ->
+%%     DR = ?depth(TreeR),
+%%     DL = ?depth(TreeL),
+%%     case DL - DR of
+%% 	2  -> lbalance(T, DR);
+%% 	-2 -> rbalance(T, DL);
+%% 	_  -> T
+%%     end.
+
+%% lbalance({K, V, _D, {KL, VL, _DL, TLL, {KLR, VLR, DLR, TLRL, TLRR}}, TR}, DR) 
+%%   when DLR =:= DR+1 -> %%LR inbalance
+%%     DLL = ?depth(TLL),
+%%     {KLR, VLR, DLL+2, {KL, VL, DLL+1, TLL, TLRL}, {K, V, DLL+1, TLRR, TR}};
+%% lbalance({Key, Val, _Depth, {KL, VL, DL, TLL, TLR}, TR}, _)  ->
+%%     {KL, VL, DL, TLL, {Key, Val, ?depth(TR)+1, TLR, TR}}.
+
+%% rbalance({K, V, _D, TL, {KR, VR, _DR, {KRL, VRL, DRL, TRLL, TRLR}, TRR}}, DL)
+%%   when DRL =:= DL+1 -> %%RL inbalance
+%%     DRR = ?depth(TRR),
+%%     {KRL, VRL, DRR+2, {K, V, DRR+1, TL, TRLL}, {KR, VR, DRR+1, TRLR, TRR}};
+%% rbalance({Key, Val, _Depth, TL, {KR, VR, DR, TRL, TRR}}, _) ->
+%%     {KR, VR, DR, {Key, Val, ?depth(TL)+1, TL, TRL}, TRR}.
+
+balance(?nil) -> ?nil; %% from after take_smallest/largest.
+balance({_Key, _Val, _Dept, TreeL, TreeR} = T) 
+  when abs(?depth(TreeL) - ?depth(TreeR)) =/= 2 ->
+    T;
+balance({Key, Val, _Depth, {KL, VL, _DL, TLL, TLR}, TR}) %% ok for TL == ?nil
+  when ?depth(TLL) =:= ?depth(TR)+1 -> %% LL inbalance
+    NewDTR = max(?depth(TR), ?depth(TLR))+1, %% optimize?
+    NewDL = max(NewDTR, ?depth(TLL))+1,
+    {KL, VL, NewDL, TLL, {Key, Val, NewDTR, TLR, TR}};
+balance({Key, Val, _Depth, TL, {KR, VR, _DR, TRL, TRR}}) 
+  when ?depth(TRR) =:= ?depth(TL)+1 -> %% RR inbalance
+    NewDTL = max(?depth(TL), ?depth(TRL))+1, %% optimize?
+    NewDR = max(NewDTL, ?depth(TRR))+1,
+    {KR, VR, NewDR, {Key, Val, NewDTL, TL, TRL}, TRR};
+balance({K, V, _D, {KL, VL, _DL, TLL, {KLR, VLR, DLR, TLRL, TLRR}}, TR}) 
+  when DLR =:= ?depth(TR)+1 -> %%LR inbalance / =:= D+1??
+    DLL = ?depth(TLL),
+    {KLR, VLR, DLL+2, {KL, VL, DLL+1, TLL, TLRL}, {K, V, DLL+1, TLRR, TR}};
+balance({K, V, _D, TL, {KR, VR, _DR, {KRL, VRL, DRL, TRLL, TRLR}, TRR}})
+  when DRL =:= ?depth(TL)+1 -> %%RL inbalance
+    DRR = ?depth(TRR),
+    {KRL, VRL, DRR+2, {K, V, DRR+1, TL, TRLL}, {KR, VR, DRR+1, TRLR, TRR}}.
+
+max(A, B) when A < B -> B;
+max(A, _) -> A.
+    
+%%done, not optimized
+insert(AKey, AVal, {Key, Val, Depth, TreeL, TreeR}) when AKey < Key ->
+    NewTreeL = insert(AKey, AVal, TreeL),
+    NewT = {Key, Val, max(?depth(NewTreeL)+1, Depth), NewTreeL, TreeR},
+    balance(NewT); 
+insert(AKey, AVal, {Key, Val, Depth, TreeL, TreeR}) when AKey > Key ->
+    NewTreeR = insert(AKey, AVal, TreeR),
+    NewT = {Key, Val, max(?depth(NewTreeR)+1, Depth), TreeL, NewTreeR},
+    balance(NewT); 
+insert(AKey, AVal, ?nil) ->
+    {AKey, AVal, 1, ?nil, ?nil}.
+
+%% TODO update/3
+%% TODO enter/3
+
+delete(DKey, {Key, Val, Depth, TreeL, TreeR}) ->
+    T = do_delete(DKey, {Key, Val, Depth, TreeL, TreeR}),
+    case is_ok(T) of
+	true ->
+	    T;
+	false ->
+	    io:format("~p ~p~n~p", [DKey, {Key, Val, Depth, TreeL, TreeR}, T])
+    end.
+
+do_delete(DKey, {Key, Val, _Depth, TreeL, TreeR}) when DKey < Key ->
+    NewTreeL = do_delete(DKey, TreeL),
+    NewT = {Key, Val, max(?depth(NewTreeL), ?depth(TreeR))+1, NewTreeL, TreeR},
+    balance(NewT);
+do_delete(DKey, {Key, Val, _Depth, TreeL, TreeR}) when DKey > Key ->
+    NewTreeR = do_delete(DKey, TreeR),
+    NewT = {Key, Val, max(?depth(TreeL), ?depth(NewTreeR))+1, TreeL, NewTreeR},
+    balance(NewT);
+do_delete(Key, {Key, _Val, _Depth, ?nil, TreeR}) -> TreeR;
+do_delete(Key, {Key, _Val, _Depth, TreeL, ?nil}) -> TreeL;
+do_delete(Key, {Key, _Val, _Depth, TreeL, TreeR}) 
+  when ?depth(TreeL) > ?depth(TreeR) ->
+    {LKey, LVal, NewTreeL} = take_largest(TreeL),
+    T = {LKey, LVal, max(?depth(NewTreeL), ?depth(TreeR))+1, NewTreeL, TreeR},
+    balance(T); %% Maybe not balance
+do_delete(Key, {Key, _Val, _Depth, TreeL, TreeR}) ->
+    {RKey, RVal, NewTreeR} = take_smallest(TreeR),
+    T = {RKey, RVal, max(?depth(TreeL), ?depth(NewTreeR))+1, TreeL, NewTreeR},
+    balance(T). %% Maybe not balance
+
+delete_any(Key, Tree) ->
+    case catch delete(Key, Tree) of
+	{'EXIT', _} -> Tree;
+	NewTree when is_tuple(NewTree) ->
+	    NewTree
+    end.
+
+%%TODO is_defined
+
+%%TODO keys
+
+%%TODO values
+
+%%TODO to_list
+
+%%TODO from_orddict
+
+
+%%tailcall?
+take_largest({Key, Val, _Depth, TreeL, ?nil}) -> {Key, Val, TreeL};
+take_largest({Key, Val, _Depth, TreeL, TreeR}) -> 
+    {LKey, LVal, NewTreeR} = take_largest(TreeR),
+    NewT0 = {Key, Val, max(?depth(TreeL), ?depth(NewTreeR))+1, TreeL, NewTreeR},
+    NewT = balance(NewT0),
+    {LKey, LVal, NewT}.
+
+take_smallest({Key, Val, _Depth, ?nil, TreeR}) -> {Key, Val, TreeR};
+take_smallest({Key, Val, _Depth, TreeL, TreeR}) -> 
+    {LKey, LVal, NewTreeL} = take_smallest(TreeL),
+    NewT0 = {Key, Val, max(?depth(NewTreeL), ?depth(TreeR))+1, NewTreeL, TreeR},
+    NewT = balance(NewT0),
+    {LKey, LVal, NewT}.
+
+
+largest({Key, Val, _Depth, _TreeL, ?nil}) -> {Key, Val};
+largest({_Key, _Val, _Depth, _TreeL, TreeR}) -> largest(TreeR).
+
+smallest({Key, Val, _Depth, ?nil, _TreeR}) -> {Key, Val};
+smallest({_Key, _Val, _Depth, TreeL, _TreeR}) -> smallest(TreeL).
+
+test_take_largest(N) ->
+    List0 = [ {X, random:uniform(N)} || X <- lists:seq(1, N) ],
+    List = lists:keysort(2, List0),
+    Insert = fun({Key, Val}, TreeAcc) -> avl:insert(Key, Val, TreeAcc) end,
+    Tree = lists:foldl(Insert, avl:empty(), List),
+    {N, _, T} = take_largest(Tree),
+    Keys = keys(T),
+    Keys = lists:seq(1, N-1).
+
+test_take_smallest(N) ->
+    List0 = [ {X, random:uniform(N)} || X <- lists:seq(1, N) ],
+    List = lists:keysort(2, List0),
+    Insert = fun({Key, Val}, TreeAcc) -> avl:insert(Key, Val, TreeAcc) end,
+    Tree = lists:foldl(Insert, avl:empty(), List),
+    {1, _, T} = take_smallest(Tree),
+    {2, _, T2} = take_smallest(T),
+    Keys = keys(T2),
+    Keys = lists:seq(3, N).
+
+
+%%TODO iterator
+
+%%TODO next
+
+%% testing (assumes no lists as keys)
+keys(T) -> lists:flatten(do_keys(T)).
+
+do_keys(?nil) -> [];
+do_keys({Key, _, _, TreeL, TreeR}) ->
+    [do_keys(TreeL), Key, do_keys(TreeR)].
+
+
+is_ok(?nil) -> true;
+is_ok({_, _, 1, ?nil, ?nil}) -> true;
+is_ok({Key, _, 2, ?nil,
+       {KeyR, _, 1, ?nil, ?nil}}) ->
+    true == (Key < KeyR);
+is_ok({Key, _, 2,
+       {KeyL, _, 1, ?nil, ?nil},
+       ?nil}) ->
+    true == (Key > KeyL);
+is_ok({Key, _, Depth, TreeL, TreeR}) ->
+    KeyL = element(1, TreeL),
+    KeyR = element(1, TreeR),
+    DepthL = ?depth(TreeL),
+    DepthR = ?depth(TreeR),
+    T = ((Key > KeyL) and
+	 (Key < KeyR) and
+	 (Depth > DepthL) and
+	 (Depth > DepthR) and
+	 ((Depth == DepthL+1) or
+	  (Depth == DepthR+1))),
+    T and is_ok(TreeL) and is_ok(TreeR).
+	    
+test() ->
+    A1 = avl:empty(),
+    true = is_empty(A1),
+    A2 = avl:insert(7, g, A1),
+    A3 = avl:insert(8, h, A2),
+    A4 = avl:insert(6, f, A3),
+    1 = avl:size(A2),
+    3 = avl:size(A4),
+    false = is_empty(A2),
+    {value, h} = lookup(8, A4),
+    {value, g} = lookup(7, A4),
+    {value, f} = lookup(6, A4),
+    is_ok(A2) and is_ok(A3) and is_ok(A4).
+    
+test_lr() ->
+    Add = fun({Key, Val}, AvlAcc) -> avl:insert(Key, Val, AvlAcc) end,
+    Stuff = [{10, "A"}, {5, "5"}, {13, "D"}, {1, "1"}, {7, "7"}, {6, "6"}],
+    A2 = lists:foldl(Add, avl:empty(), Stuff),
+    io:format("A2 ~p~n~n", [A2]),
+    is_ok(A2).
+
+
+test_del() ->
+    AddF = fun({Key, Val}, AvlAcc) -> insert(Key, Val, AvlAcc) end,
+    DelF = fun(Key, AvlAcc) -> delete(Key, AvlAcc) end,
+    Stuff = [{10, "A"}, {5, "5"}, {13, "D"}, {1, "1"}, {7, "7"}, {6, "6"}],
+    T = lists:foldl(AddF, empty(), Stuff),
+    [1, 6, 7, 10, 13] = keys(delete(5, T)),
+    [1, 5, 6, 7, 13] = keys(delete(10, T)),
+    DelStuff = [1, 6, 10],
+    T2 = lists:foldl(DelF, T, DelStuff),
+    true = (?depth(T2) == ?depth(T)-1),
+    T = delete_any(mojs, T),
+    is_ok(T) and is_ok(T2).
+    
+test_all() ->
+    test() and test_lr() and test_del().
+	    
+
+
---
+++ avl_bench.erl
@@ -0,0 +1,65 @@
+%%%-------------------------------------------------------------------
+%%% File    : bench.erl
+%%% Author  : Andreas Hasselberg <andreas.hasselberg@gmail.com>
+%%% Description : Mojselimojs
+%%%
+%%% Created : 15 Jan 2008 by Andreas Hasselberg <>
+%%%-------------------------------------------------------------------
+-module(avl_bench).
+
+-compile([export_all]).
+
+bench() ->
+    RandTimes = do_bench(10000, rand),
+    SortTimes = do_bench(10000, sort),
+    io:format("rand ~p", [RandTimes]),
+    io:format("sort ~p", [SortTimes]).
+
+clock__time() ->
+    {element(2, statistics(runtime)), element(2, statistics(wall_clock))}.
+
+do_bench(N, R) ->
+    Keys = case R of
+	       rand -> Rand = [ {random:uniform(N),X} || X <- lists:seq(1, N) ],
+		       [ X || {_,X} <- lists:sort(Rand)];
+	       sort -> lists:seq(1, N)
+	   end,
+    Values = lists:seq(1, N*2, 2),
+    List = lists:zip(Keys, Values),
+    AvlTimes = do_avl(List),
+    GbTimes = do_gb(List),
+    {AvlTimes, GbTimes}.
+
+do_avl(List) ->
+    %% insert
+    {_, _} = clock__time(),
+    Insert = fun({Key, Val}, TreeAcc) -> avl:insert(Key, Val, TreeAcc) end,
+    Tree = lists:foldl(Insert, avl:empty(), List),
+    {Rtime1, Wall1} = clock__time(),
+    %% lookup
+    Lookup = fun({Key, Val}) -> {value, Val} == avl:lookup(Key, Tree) end,
+    true = lists:all(Lookup, List),
+    {Rtime2, Wall2} = clock__time(),
+    %% delete
+    Delete = fun({Key, _Val}, TreeAcc) -> avl:delete(Key, TreeAcc) end,
+    lists:foldl(Delete, Tree, List),
+    {Rtime3, Wall3} = clock__time(),
+    {Rtime1, Rtime2, Rtime3}.
+    
+
+do_gb(List) ->
+    %% insert
+    {_, _} = clock__time(),
+    Insert = fun({Key, Val}, TreeAcc) -> gb_trees:insert(Key, Val, TreeAcc) end,
+    Tree = lists:foldl(Insert, gb_trees:empty(), List),
+    {Rtime1, Wall1} = clock__time(),
+    %% lookup
+    Lookup = fun({Key, Val}) -> {value, Val} == gb_trees:lookup(Key, Tree) end,
+    true = lists:all(Lookup, List),
+    {Rtime2, Wall2} = clock__time(),
+    %% delete
+    Delete = fun({Key, _Val}, TreeAcc) -> gb_trees:delete(Key, TreeAcc) end,
+    lists:foldl(Delete, Tree, List),
+    {Rtime3, Wall3} = clock__time(),
+    {Rtime1, Rtime2, Rtime3}.
+
